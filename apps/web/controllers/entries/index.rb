c224ddb4f13e9d8555dfb10eb7e5e819aceed1e6
bkjung <bkjung94@nearthlab.com>
2017-11-09 20:53:17 +0900

    test1

--- apps/web/controllers/entries/index.rb
+++ apps/web/controllers/entries/index.rb
@@ -3,11 +3,17 @@ module Web::Controllers::Entries
     include Web::Action
 
     expose :entries
+    expose :sort  # not really necessary anymore
 
     def call(params)
-      # @entries = EntryRepository.new.all
-      @entries = EntryRepository.new.most_recent
-      # @entries = EntryRepository.new.least_recent
+      @sort = parmas[:sort]
+      
+      # most_recent or least_recent
+      # TODO: Clean this up
+      if @sort == 'most_recent'
+        @entries = EntryRepository.new.most_recent
+      else
+        @entries = EntryRepository.new.least_recent
     end
   end
 end
